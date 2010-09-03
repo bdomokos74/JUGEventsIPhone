77a87954a4c3f098cc2a782c191abc56d550731f
Balint Domokos <bdomokos74@yahoo.com>
2010-09-03 21:01:19 +0200

    Add unit testing support

---
+++ EventServiceTest.h
@@ -0,0 +1,15 @@
+//
+//  EventServiceTest.h
+//  JugEventsIPhone
+//
+//  Created by Balint Domokos on 03/09/2010.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+
+@interface EventServiceTest : SenTestCase {
+}
+@end
